b906722e5b4e2495fc6ba991e2ca8242adc503ef
Dan Neira <danneira@Mac.lan>
2025-07-25 12:10:00 -0700

    Redirect root to /users and stub Users page

---
+++ apps/admin/app/users/page.tsx
@@ -0,0 +1,11 @@
+// apps/admin/app/users/page.tsx
+"use client";
+import "../styles/userList.style.css";
+
+export default function UsersPage() {
+  return (
+    <div className="user-list-container">
+      <h2>Users List Goes Here</h2>
+    </div>
+  );
+}
